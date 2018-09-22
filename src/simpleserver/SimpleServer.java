9f10ab7efc9f6e862903963cb0b7a0cd62c35fa5
pyaenaing <43054337+pyaenaing@users.noreply.github.com>
2018-09-21 21:09:07 -0700

    clean up

--- src/simpleserver/SimpleServer.java
+++ src/simpleserver/SimpleServer.java
@@ -6,66 +6,76 @@ import java.net.Socket;
 
 class SimpleServer {
 
-  public static void main(String[] args) throws IOException {
-    ServerSocket ding;
-    Socket dong = null;
-    String resource = null;
-    try {
-      ding = new ServerSocket(1299);
-      System.out.println("Opened socket " + 1299);
-      while (true) {
-
-        // keeps listening for new clients, one at a time
+    public static void main(String[] args) throws IOException {
+        ServerSocket ding;
+        Socket dong = null;
+        //String resource = null;
         try {
-          dong = ding.accept(); // waits for client here
-        } catch (IOException e) {
-          System.out.println("Error opening socket");
-          System.exit(1);
-        }
+            ding = new ServerSocket(1299);
+            System.out.println("Opened socket " + 1299);
+            while (true) {
 
-        InputStream stream = dong.getInputStream();
-        BufferedReader in = new BufferedReader(new InputStreamReader(stream));
-        try {
+                String parceString[];
+                String resource = null;
+                // keeps listening for new clients, one at a time
+                try {
+                    dong = ding.accept(); // waits for client here
+                } catch (IOException e) {
+                    System.out.println("Error opening socket");
+                    System.exit(1);
+                }
 
-          // read the first line to get the request method, URI and HTTP version
-          String line = in.readLine();
-          System.out.println("----------REQUEST START---------");
-          System.out.println(line);
-          // read only headers
-          line = in.readLine();
-          while (line != null && line.trim().length() > 0) {
-            int index = line.indexOf(": ");
-            if (index > 0) {
-              System.out.println(line);
-            } else {
-              break;
+                InputStream stream = dong.getInputStream();
+                BufferedReader in = new BufferedReader(new InputStreamReader(stream));
+                try {
+
+                    // read the first line to get the request method, URI and HTTP version
+                    String line = in.readLine();
+
+                    System.out.println("----------REQUEST START---------");
+
+                    parceString = line.split(" ");
+                    resource = parceString[1].substring(1);
+
+                    System.out.println(line);
+                    // read only headers
+                    line = in.readLine();
+                    while (line != null && line.trim().length() > 0) {
+                        int index = line.indexOf(": ");
+                        if (index > 0) {
+                            System.out.println(line);
+                        } else {
+                            break;
+                        }
+                        line = in.readLine();
+                    }
+                    System.out.println("----------REQUEST END---------\n\n");
+                } catch (IOException e) {
+                    System.out.println("Error reading");
+                    System.exit(1);
+                }
+
+                BufferedOutputStream out = new BufferedOutputStream(dong.getOutputStream());
+                PrintWriter writer = new PrintWriter(out, true);  // char output to the client
+
+                // every response will always have the status-line, date, and server name
+                writer.println("HTTP/1.1 200 OK");
+                writer.println("Server: TEST");
+                writer.println("Connection: close");
+                writer.println("Content-type: text/html");
+                writer.println("");
+
+                // Body of our response
+                if (resource.equals("hello"))
+                    writer.println("<h1>Hello World</h1>");
+                else
+                writer.println("<h1>Some cool response!</h1>");
+
+                dong.close();
             }
-            line = in.readLine();
-          }
-          System.out.println("----------REQUEST END---------\n\n");
         } catch (IOException e) {
-          System.out.println("Error reading");
-          System.exit(1);
+            System.out.println("Error opening socket");
+            System.exit(1);
         }
-
-        BufferedOutputStream out = new BufferedOutputStream(dong.getOutputStream());
-        PrintWriter writer = new PrintWriter(out, true);  // char output to the client
-
-        // every response will always have the status-line, date, and server name
-        writer.println("HTTP/1.1 200 OK");
-        writer.println("Server: TEST");
-        writer.println("Connection: close");
-        writer.println("Content-type: text/html");
-        writer.println("");
-
-        // Body of our response
-        writer.println("<h1>Some cool response!</h1>");
-
-        dong.close();
-      }
-    } catch (IOException e) {
-      System.out.println("Error opening socket");
-      System.exit(1);
     }
-  }
 }
