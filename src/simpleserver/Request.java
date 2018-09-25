10a1cb0e90f0491777674b9395de87c775d24fb2
pyaenaing <43054337+pyaenaing@users.noreply.github.com>
2018-09-24 18:50:39 -0700

    Request update

--- src/simpleserver/Request.java
+++ src/simpleserver/Request.java
@@ -3,10 +3,63 @@ package simpleserver;
 public class Request {
 
     private String endpoint;
+    private String request;
     private int id = -1;
     private int length = 0;
     private boolean valid = false;
+    private boolean returnall = false;
+    public String errorr = "";
 
+    public Request(String request)
+    {
+        parse(request);
+    }
+
+    public void parse(String request)
+    {
+        if(request.equals("user"))
+        {
+            returnall = valid = true;
+            return;
+        }
+
+        try {
+            String[] Split = request.split("\\?");
+            endpoint = Split[0];
+
+            if (endpoint.equals("user") || endpoint.equals("posts")) {
+                Split = Split[1].split("&");
+                if (Split.length == 1) {
+                    Split = Split[0].split("=");
+                    if (Split[0].equals("userid") || Split[0].equals("postid")) {
+                        id = Integer.valueOf(Split[1]);
+                    }
+                } else if (Split.length == 2) {
+                    String[] temp1, temp2;
+                    temp1 = Split[0].split("=");
+                    temp2 = Split[1].split("=");
+                    if (temp1[0].equals("postid") && temp2[0].equals("maxlength")) {
+                        id = Integer.valueOf(temp1[1]);
+                        length = Integer.valueOf(temp2[1]);
+                    } else if (temp2[0].equals("postid") && temp1[0].equals("maxlength")) {
+                        id = Integer.valueOf(temp2[1]);
+                        length = Integer.valueOf(temp1[1]);
+                    } else
+                        throw new Exception();
+                }
+            }
+            else
+                throw new Exception();
+
+        }catch(Exception e)
+        {
+
+            errorr = e.toString();
+            valid = false;
+        }
+    }
+
+    // *** Getters ***
     public String getEndpoint()
     {
         return this.endpoint;
@@ -23,10 +76,9 @@ public class Request {
     {
         return this.valid;
     }
-
-    public Request(String request)
+    public boolean getReturnall()
     {
-        String [] splitted = request.split(" ");
+        return this.returnall;
     }
-
 }
+
--- src/simpleserver/testing.java
+++ src/simpleserver/testing.java
@@ -3,9 +3,19 @@ package simpleserver;
 public class testing {
     public static void main(String args[])
     {
-        String apple = "/apple";
-        apple = apple.substring(1);
-            System.out.println(apple);
 
+       Request r = new Request("posts?postid=7");
+       //Request r = new Request("user");
+
+
+       if (r.getValid())
+           System.out.println("It is Valid");
+       if (!r.getValid())
+           System.out.println(r.errorr);
+       System.out.println("Endpoint = " + r.getEndpoint());
+       System.out.println("ID = " + r.getID());
+       System.out.println("Length = " + r.getLength());
+       System.out.println("Return all = " + r.getReturnall());
+       /**/
     }
 }
